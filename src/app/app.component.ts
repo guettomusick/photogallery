50046add201c1536208377923d036bb0457c3f17
Fernando Caamaño <fcaamano@qonico.com>
2018-02-28 13:59:12 -0300

    revision 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Album {
+  albumId: number,
+  id: number,
+  title: string,
+  url: string,
+  thumbnailUrl: string
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +16,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  albums: any = [];
+
+  constructor(private http: HttpClient) {
+    this.http.get<Album[]>('https://jsonplaceholder.typicode.com/photos').subscribe(data => {
+     this.albums = data
+     .reduce((pv: any[], album) => {
+       if(!pv[album.albumId]) {
+        pv[album.albumId] = [];
+       };
+       pv[album.albumId][album.id] = {title: album.title, url: album.url, thumbnailUrl: album.thumbnailUrl};
+       return pv;
+     }, [])
+     .slice(-3)
+     .map(album => album.slice(-2));
+    });
+  }
 }
